a0395156adef14f8f7fa528688cee06ea7838525
Rishikesh Phatangare <56035442+riship251297@users.noreply.github.com>
2022-06-06 22:24:26 -0400

    Time: 59 ms (13.21%), Space: 13.8 MB (62.51%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.py
@@ -0,0 +1,19 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        if needle == '':
+            return 0
+        if needle not in haystack:
+            return -1
+        if needle in haystack:
+            haystack = haystack.replace(needle,'.')
+            
+            return haystack.index('.')
+        
+        
+       
+    # if needle in haystack:
+#         haystack = haystack.replace(needle,"_")
+#         return haystack.index("_")
+#     return -1
+        
