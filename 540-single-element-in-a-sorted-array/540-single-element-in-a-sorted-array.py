64b043157f303914633dfa500720381ff73d36e0
Rishikesh Phatangare <56035442+riship251297@users.noreply.github.com>
2022-06-07 08:02:32 -0400

    Time: 373 ms (5.01%), Space: 26.1 MB (5.36%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        
+        freq = {}
+        
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        
+        for k,v in freq.items():
+            if v == 1:
+                return k
+        
