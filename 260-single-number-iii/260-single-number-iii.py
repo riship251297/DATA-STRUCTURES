1c8719aae48127e8fe79b109060cb020abc64440
Rishikesh Phatangare <56035442+riship251297@users.noreply.github.com>
2022-10-04 20:35:34 -0400

    Time: 112 ms (53.50%), Space: 16.2 MB (35.11%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,16 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        freq = {}
         
+        freq = {}
         for i in nums:
             if i in freq:
                 freq[i] += 1
             else:
                 freq[i] = 1
-        op = []       
-        for keys,values in freq.items():
+        
+        once = []
+        for key,values in freq.items():
             if values == 1:
-                op.append(keys)
-                
-        return op
+                once.append(key)
+        
+        return once
         
