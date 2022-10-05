e64ea417ae0f13ad5dedb0a000332aae37c65bd0
Rishikesh Phatangare <56035442+riship251297@users.noreply.github.com>
2022-10-04 20:24:34 -0400

    Time: 368 ms (20.81%), Space: 15.6 MB (10.61%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,10 +2,15 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
         freq = {}
-        
         for i in nums:
             freq[i] = 1
-            
-        for i in range(0,len(nums)+1):
-            if i not in freq:
-                return i
+        
+        missing = []
+        for i in range(len(nums)+1):
+            if i in freq:
+                pass
+            else:
+                missing.append(i)
+                
+        return missing[0]
+        
