3cf28c093ac5e7a48d8e14417d4318f3812b270a
Rishikesh Phatangare <56035442+riship251297@users.noreply.github.com>
2022-10-04 20:33:46 -0400

    Time: 105 ms (65.76%), Space: 16.2 MB (7.52%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq = {}
         
+        freq = {}
         for i in nums:
             if i in freq:
                 freq[i] += 1
             else:
                 freq[i] = 1
-                
-        for keys,values in freq.items():
+            
+        for key,values in freq.items():
             if values == 1:
-                return keys
+                return key
         
