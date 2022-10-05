8c8061d85bb51a4ad5760c85ba2ce4ea463a9731
Rishikesh Phatangare <56035442+riship251297@users.noreply.github.com>
2022-10-04 22:47:36 -0400

    Time: 465 ms (74.78%), Space: 23.8 MB (50.44%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -2,14 +2,15 @@ class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         
         freq = {}
-        
         for i in nums:
             freq[i] = 1
             
-        op = []
-        for i in range(1,len(nums) + 1):
-            if i not in freq:
-                op.append(i)
-                
-        return op
+        ele = []
+        for i in range(1,len(nums)+1):
+            if i in freq:
+                pass
+            else:
+                ele.append(i)
+            
+        return ele
         
